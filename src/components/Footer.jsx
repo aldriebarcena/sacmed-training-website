154f535e5a27d5b083ac0a3550af02d19bb29904
aldriebarcena <aldrievbarcena@gmail.com>
2024-12-15 21:24:47 -0800

    Add padding under header

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,13 +8,13 @@ const Footer = () => {
         <Image className="w-[10%] pb-20" src={Logo} alt="Sacmed Logo" />
         <div className="flex flex-row space-x-24 justify-end">
           <div>
-            <div className="font-roboto font-bold">SacMed Training, LLC</div>
+            <div className="font-roboto font-bold pb-2">SacMed Training, LLC</div>
             <div className="font-roboto text-sm">3443 Ramona Ave, Suite 25</div>
             <div className="font-roboto text-sm">Sacramento, CA 95826</div>
             <div className="font-roboto text-sm">Tel: (916) 226-5427</div>
           </div>
           <div>
-            <div className="font-roboto font-bold">Main Office Hours</div>
+            <div className="font-roboto font-bold pb-2">Main Office Hours</div>
             <div className="font-roboto text-sm">Monday - Friday</div>
             <div className="font-roboto text-sm">10AM - 3PM</div>
             <div className="font-roboto text-sm">Closed Weekends</div>
